6b4dc71634535a7b3bdc4ab2b36cc5421b86fb93
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2024-12-18 20:05:42 +0100

    build: create poetry build script for pyinstaller, add icon

---
+++ spectrexcel/pyinstaller.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from PyInstaller.__main__ import run as pyinstaller_run
+
+
+def install():
+    pyinstaller_run(
+        [
+            str(Path(__file__).parent.absolute() / "main.py"),
+            "--name",
+            "spectrexcel",
+            "--onefile",
+            "--noconsole",  # don't open console window
+            "--optimize",
+            "0",
+            "--icon",
+            "../icon.ico",
+        ]
+    )
